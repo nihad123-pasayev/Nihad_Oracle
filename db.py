ceaed579760170613fe6db3e7337dedf70155cb7
nihad123-pasayev <nihatpasayev386@gmail.com>
2026-03-13 14:43:22 +0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,31 @@
+import oracledb
+
+def get_connection():
+    return oracledb.connect(
+        user='nihad1',
+        password='12345',
+        dsn='localhost/orcl1')
+def create_table():
+    connection = get_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("""
+            CREATE TABLE employees(
+                emp_id       INT PRIMARY KEY,
+                full_name    VARCHAR2(50),
+                salary       INT,
+                dept_id      INT)
+        """)
+        connection.commit()
+        print("Table created successfully")
+    except Exception as e:
+        if "ORA-00955" in str(e):
+            print("Table already exists")
+        else:
+            print(f"Error: {e}")
+    finally:
+        cursor.close()
+        connection.close()
+
+
+
